05994dce30d6708445d268cf4d07c7dd15aef7f5
이원형 <67450169+whl0526@users.noreply.github.com>
2024-03-06 22:09:20 +0900

    Initial Commit

---
+++ ReelsCamera/ReelsCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelsCameraApp.swift
+//  ReelsCamera
+//
+//  Created by 이원형 on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ReelsCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
